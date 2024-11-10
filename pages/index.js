b509e77cca49c96424d227ade589941517f60a21
Tiago Muniz <muniz_tii@hotmail.com>
2024-11-10 00:19:39 -0300

    Adiciona index.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default home
+
+function home() {
+    return <main>
+        <h1>Giulia, eu te amo sua lindeza! ❤️ <br></br>
+            e isso vai ficar no meu GitHub. 🫰</h1>
+        <br></br>
+        <br></br>
+        <br></br>
+        <h4>Agora você tem um pedaço para você e somente para você na internet. 🥰</h4>
+    </main>
+} 
